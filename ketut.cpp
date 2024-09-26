13917cc176e968140469560a22c8d2ea385a5826
GIbrannn-commits <gibranalgi69fhari@gmail.com>
2024-09-26 11:48:10 +0700

    Add files via upload

---
+++ ketut.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int i,N,jumlah=0;
+N=0;
+
+cout << "masukkan banyak nilai :  ";
+cin >> i ;
+jumlah = jumlah+i;
+if (i%2 ==0){
+	cout << i << " - ";
+	i=i-2;
+}
+while(i>N){
+	if (i%2 ==0){
+			cout << i << " - ";
+	jumlah = jumlah-(i);
+	i;
+	}
+	i--;
+}
+jumlah = jumlah-(i);
+cout << i << " ";
+cout << " = " << jumlah << endl;
+}
+
